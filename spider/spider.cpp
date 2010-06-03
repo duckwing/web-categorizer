25997e97831220a49b7fdd90b177da21d480b371
jin <jin@localhost>
2010-06-03 17:24:03 +0300

    auto

--- spider/spider.cpp
+++ spider/spider.cpp
@@ -16,23 +16,23 @@ int main(int argc, char** argv){
     CDbc                    *dbc = new CDbc();
 
     // connect
-    QUEUED_CONNECT(&web, started, &dbc, start, ());
-    QUEUED_CONNECT(&worker, finished, &web, deleteLater, ());
+    QUEUED_CONNECT(web, started, dbc, start, ());
+    QUEUED_CONNECT(&worker, finished, web, deleteLater, ());
 
-    DIRECT_CONNECT(&dbc, destroyed, &worker, quit, ());
-    DIRECT_CONNECT(&web, destroyed, &app, quit, ());
+    DIRECT_CONNECT(dbc, destroyed, &worker, quit, ());
+    DIRECT_CONNECT(web, destroyed, &app, quit, ());
 
-    QUEUED_CONNECT(&dbc, request, &web, request, (CRequest*));
-    QUEUED_CONNECT(&web, reply  , &dbc, reply  , (CRequest*));
+    QUEUED_CONNECT(dbc, request, web, request, (CRequest*));
+    QUEUED_CONNECT(web, reply  , dbc, reply  , (CRequest*));
 
     // move dbc to another thread
-    dbc.moveToThread(&worker);
+    dbc->moveToThread(&worker);
 
     // start the thread
     worker.start();
 
     // send the start signal to dbc
-    web.start();
+    web->start();
 
     // exec local event loop
     QCoreApplication::exec();
